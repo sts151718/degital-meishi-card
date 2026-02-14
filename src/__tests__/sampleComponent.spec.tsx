664188dd4a3fc595e2dbe5b1942767adbeb1d4de
sts151718 <tsubokurajob151718@gmail.com>
2026-02-14 11:51:48 +0900

    Vitest設定確認用のテストコードを削除

--- src/__tests__/sampleComponent.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import App from "@/App";
-
-describe("Vitest動作確認テスト", () => {
-  it("タイトルが「TEST TITLE」であること", () => {
-    render(<App />);
-  });
-});
