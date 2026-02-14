be67746c35c595f8a3de1855f396c6bd146c7cc8
sts151718 <tsubokurajob151718@gmail.com>
2026-02-14 12:55:41 +0900

    Chakra UI 導入

---
+++ src/components/ui/provider.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import {
+  ChakraProvider,
+  createSystem,
+  defaultConfig,
+  defineConfig,
+} from "@chakra-ui/react";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+
+const config = defineConfig({
+  globalCss: {
+    "body,html": {
+      margin: 0,
+      padding: 0,
+      color: "gray.600",
+    },
+  },
+});
+
+const system = createSystem(defaultConfig, config);
+
+export function Provider(props: ColorModeProviderProps) {
+  return (
+    <ChakraProvider value={system}>
+      <ColorModeProvider {...props} />
+    </ChakraProvider>
+  );
+}
